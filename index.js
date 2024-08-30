38fadb736358722b9d1a8b152f827827500f7a57
Dmytro Iovenko <iovenko@gmail.com>
2024-08-30 16:05:55 -0400

    add custom routes

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+var menuLinks = [
+  { text: "about", href: "/about" },
+  {
+    text: "catalog",
+    href: "/catalog",
+  },
+  {
+    text: "orders",
+    href: "/orders",
+  },
+  {
+    text: "account",
+    href: "/account",
+  },
+];
+
 const http = require("http");
 
 const hostname = "127.0.0.1";
@@ -5,9 +21,34 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
+
   res.setHeader("Content-Type", "text/html");
-  res.write('<h1 style="color: red">Hello World!</h1>');
-  res.write("<p>I wonder what else we can send...</p>");
+
+  res.write('<nav style="position: relative; margin-bottom: 1rem;">');
+  menuLinks.forEach((item) => {
+    res.write(
+      `<a style="padding: 0 1rem;text-transform: uppercase;text-decoration: none;" href="${item.href}" target="_blank">${item.text}</a>`
+    );
+  });
+  res.write("</nav>");
+  console.log(req.url);
+  switch (req.url) {
+    case "/about":
+      res.write("<h1>About page</h1>");
+      break;
+    case "/catalog":
+      res.write("<h1>Catalog page</h1>");
+      break;
+    case "/orders":
+      res.write("<h1>Orders page</h1>");
+      break;
+    case "/account":
+      res.write("<h1>Account page</h1>");
+      break;
+    default:
+      res.write('<h1 style="color: red">Hello World!</h1>');
+      res.write("<p>I wonder what else we can send...</p>");
+  }
   res.end();
 });
 
